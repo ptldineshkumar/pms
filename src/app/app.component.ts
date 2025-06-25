b7e444713e9b35c482628ae534a499ee8a812ff9
ptldineshkumar <dineshkumar.b@pointelsolutions.com>
2025-06-25 17:18:09 +0530

    dynamic form implementede

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'pms';
+  form: FormGroup
+  constructor() {
+    this.form = new FormGroup({
+      contacts: new FormArray([
+        new FormControl('')
+      ])
+    })
+  }
+  removeContact(index: number): void {
+    if (this.contacts.length > 1) {
+      this.contacts.removeAt(index)
+    }
+
+  }
+  addContacts(): void {
+    this.contacts.push(new FormControl(''))
+  }
+  get contacts(): FormArray {
+
+    
+    return this.form.get('contacts') as FormArray;
+  }
 }
